e3a628dee616b7aa595d6baa6941da97853e1c5c
Roshan Krishna A S <109686335+ASRoshanKrishna@users.noreply.github.com>
2025-06-19 08:39:53 +0530

    Add application properties, connect DB and Test Run

--- src/main/java/com/transfolio/transfolio/TransfolioApplication.java
+++ src/main/java/com/transfolio/transfolio/TransfolioApplication.java
@@ -2,12 +2,19 @@ package com.transfolio.transfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TransfolioApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TransfolioApplication.class, args);
 	}
 
+	@GetMapping
+	public static String printHello() {
+		return "Hello World!";
+	}
 }
