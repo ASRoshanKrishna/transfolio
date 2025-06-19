6435bc255044f8c4027e54453cbf5c4d320e4248
Roshan Krishna A S <109686335+ASRoshanKrishna@users.noreply.github.com>
2025-06-19 07:48:31 +0530

    Initial Spring Boot setup

---
+++ src/main/java/com/transfolio/transfolio/TransfolioApplication.java
@@ -0,0 +1,13 @@
+package com.transfolio.transfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/transfolio/transfolio/TransfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transfolio.transfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
